903245460013406f2793b273c30722286e367e79
Lbaruch <aviadam.segel@gmail.com>
2022-02-03 14:18:41 +0200

    4.04

--- src/lib/components/lev/fiappru.svelte
+++ src/lib/components/lev/fiappru.svelte
@@ -398,9 +398,9 @@ title="ביטול"
          
             <p style="margin: 7px;"><span style="color:green" title="בעד">{noofusersOk} </span><span style="color:aqua" title="לא הצביעו">{noofusersWaiting} </span><span style="color:var(--barbi-pink)" title="נגד">{noofusersNo} </span></p>
             {#if !already}
-            <button1 on:click={agree} style="margin: 0;" class = "btn" name="requestToJoin"><i class="far fa-check-circle"></i></button1>
+            <button1 on:click={agree} style="margin: 0;" class = "btn" name="requestToJoin"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" /></svg></button1>
             <button3 on:click= {ask} style="margin: 0;" class = "btn" name="negotiate"><i class="far fa-comments"></i></button3>
-            <button2 on:click={open} style="margin: 0;" class = "btn"name="decline"><i class="far fa-times-circle"></i></button2>
+            <button2 on:click={open} style="margin: 0;" class = "btn"name="decline"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,13H7V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg></button2>
         {/if}
         </div>
         {/if}
