da8e92a0e0ca429908b34f03d2d816fe293bef36
Lbaruch <aviadam.segel@gmail.com>
2022-02-24 14:01:11 +0200

    7.5

---
+++ src/lib/components/screens/NotFoundScreen.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from "../header/header.svelte"
+</script>
+<Header/>
+<div class="grid items-center text-center justify-center aling-center bg-black" style="min-height: 100vh;">
+<h1 class="text-barbi text-xl">404</h1>
+<h2 class="text-barbi text-sm">הדף לא נמצא</h2>
+<img style="width:100vw:" alt="error" src="https://res.cloudinary.com/love1/image/upload/v1645703329/error1_t2fvd8.jpg">
+</div>
