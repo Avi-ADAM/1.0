554f2a976bdc3c3a46bd93f2aa5c2d176119483a
Lbaruch <aviadam.segel@gmail.com>
2022-04-01 17:06:37 +0300

    18.2

--- src/routes/me/__layout.reset.svelte
+++ src/routes/me/__layout.reset.svelte
@@ -25,15 +25,25 @@ function login () {
 {#if isAuthed}
     <slot />
 {:else}
-<div class="a">
-<h1 class="text-barbi text-xl ">הגעת לעמוד הדורש הרשמה</h1>
-<button class="bg-gradient-to-br hover:from-gra hover:via-grb hover:via-gr-c hover:via-grd hover:to-gre from-barbi to-mpink  text-gold hover:text-barbi font-bold m-2 py-2 px-4 rounded-full" on:click={reg}>להרשמה</button>
-<button class="bg-gradient-to-br hover:from-gra hover:via-grb hover:via-gr-c hover:via-grd hover:to-gre from-barbi to-mpink  text-gold hover:text-barbi font-bold m-2 py-2 px-4 rounded-full" on:click={login}>להתחברות</button>
-</div>
+<div class="a  bg-gradient-to-br from-gra to-grb">
+    <div class="b border border-barbi">
+<h1 class="text-lturk font-bold text-2xl p-2">הגעת לעמוד הדורש הרשמה</h1>
+<div class="flex flex-row flex-auto justify-between">
+<button class=" m-2 border border-gold hover:border-barbi bg-gradient-to-br hover:from-gra hover:via-grb hover:via-gr-c hover:via-grd hover:to-gre from-barbi to-mpink  text-gold hover:text-barbi font-bold  py-2 px-4 rounded-full" on:click={reg}>להרשמה</button>
+<button class="m-2 border border-gold hover:border-barbi bg-gradient-to-br hover:from-gra hover:via-grb hover:via-gr-c hover:via-grd hover:to-gre from-barbi to-mpink  text-gold hover:text-barbi font-bold  py-2 px-4 rounded-full" on:click={login}>להתחברות</button>
+</div></div></div>
 {/if}
 <style>
-    .a{
+    .b{
         display: grid;
+        align-items: center;
+        justify-content: center;
+    }
+    .a{
+      
+        height: 100vh;
+        width: 100vw;
+         display: grid;
         align-items: center;
         justify-content: center;
     }
