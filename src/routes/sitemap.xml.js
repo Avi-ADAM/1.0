18ba851b03564fcd2e2c6d4a578d2fb6b8fb8f5a
Lbaruch <aviadam.segel@gmail.com>
2022-06-29 19:53:18 +0300

    44.5

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,34 +1,20 @@
-import { url, pages } from '$lib/constants'; // or wherever your relevant data is
-
-export const get = async () => {
-    const headers = {
-        'Cache-Control': 'max-age=0, s-maxage=3600',
-        'Content-Type': 'application/xml'
-    };
-    const body = `<?xml version="1.0" encoding="UTF-8" ?>
-  <urlset
-    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-  >
-    <url>
-      <loc>${url}</loc>
-      <changefreq>always</changefreq>
-      <priority>1.0</priority>
-    </url>
-    ${pages
-            .map(
-                (page) => `
-    <url>
-      <loc>${url}/${page}</loc>
-      <changefreq>daily</changefreq>
-      <priority>0.5</priority>
-    </url>
-    `
-            )
-            .join('')}
-  </urlset>`
-
+export async function get() {
     return {
-        headers,
-        body
+        headers: {
+            'Content-Type': 'application/xml'
+        },
+        body: `
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <!-- <url> elements go here -->
+      </urlset>
+    `.trim()
     };
-};
+}
