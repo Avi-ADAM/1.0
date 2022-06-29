4a800bda4d23e70cb4535e2c2d43f7935d1b762b
Lbaruch <aviadam.segel@gmail.com>
2022-06-30 00:02:01 +0300

    44.6

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -13,6 +13,43 @@ export async function get() {
         xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
         xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
       >
+      <url>
+            <loc>https://www.1lev1.world/</loc>
+            <lastmod>2022-06-29T20:57:22+00:00</lastmod>
+            <priority>1.00</priority>
+            <changefreq>weekly</changefreq>
+            </url>
+            <url>
+            <loc>https://www.1lev1.world/login</loc>
+            <lastmod>2022-06-29T20:57:22+00:00</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.99</priority>
+            </url>
+            <url>
+            <loc>https://shalom.1lev1.world/</loc>
+            <lastmod>2022-06-12T23:42:27+00:00</lastmod>
+            <priority>0.98</priority>
+            </url>
+            <url>
+            <loc>https://shalom.1lev1.world/1-0/</loc>
+            <lastmod>2022-06-12T23:42:27+00:00</lastmod>
+            <priority>0.90</priority>
+            </url>
+            <url>
+            <loc>https://shalom.1lev1.world/l/</loc>
+            <lastmod>2022-06-12T23:42:27+00:00</lastmod>
+            <priority>0.80</priority>
+            </url>
+            <url>
+            <loc>https://shalom.1lev1.world/1/</loc>
+            <lastmod>2022-06-12T23:42:27+00:00</lastmod>
+            <priority>0.80</priority>
+            </url>
+            <url>
+            <loc>https://shalom.1lev1.world/harmoni/</loc>
+            <lastmod>2022-06-12T23:42:27+00:00</lastmod>
+            <priority>0.80</priority>
+            </url>
         <!-- <url> elements go here -->
       </urlset>
     `.trim()
