45ab81ad8dfe534fb822d3987ee2f2f0753ec482
Lbaruch <aviadam.segel@gmail.com>
2022-07-13 20:27:56 +0300

    46.2 addnewval i18n

--- src/lib/components/addnew/addnewval.svelte
+++ src/lib/components/addnew/addnewval.svelte
@@ -1,8 +1,7 @@
 <script>
-  //  import { Textarea, TextField, MaterialAppMin } from 'svelte-materialify';
-    import axios from 'axios';
      import { createEventDispatcher } from 'svelte';
  const dispatch = createEventDispatcher();
+           import { lang } from '$lib/stores/lang.js'
  let vallues = [];
 // דף של השראה כפתור להוסיף את ההשראה שלך, כפתור לתיקונים  
     let name_value;
@@ -73,12 +72,17 @@ function dispatchb () {
 };
 export let addS = false; 
 let cencel = 'ביטול';
+const addn = {"he":"הוספת ערך חדש","en": "Add new Vallue"}
+const valn = {"he":"שם הערך", "en": "Vallue name"}
+const des = {"he": "תיאור קצר", "en": "Vallue short description"}
+const btnTitles = {"he": "הוספה", "en": "Add"}
+const errmsg = {"he": "השם כבר קיים","en":"name already exists"}
  </script>
  <div style="--the:{`var(${color})`};">
 {#if addS == false}
 <button 
 class="border border-barbi hover:border-gold bg-gradient-to-br from-gra via-grb via-gr-c via-grd to-gre hover:from-barbi hover:to-mpink text-barbi hover:text-gold font-bold py-2 px-2 rounded-full"
-on:click={() => addS = true}>הוספת ערך שאינו ברשימה</button>
+on:click={() => addS = true}>{addn[$lang]}</button>
 {:else}
 <button title={cencel}
 on:click={dispatchb}
@@ -87,23 +91,23 @@ class=" hover:bg-barbi text-gold hover:text-lturk font-bold  rounded-full text-c
 </svg></button>
 
 <div >
- <h1 class="font-bold" style="font-size: 1rem; line-height: normal; color: var(--barbi-pink); "> הוספת ערך חדש</h1>    
+ <h1 class="font-bold" style="font-size: 1rem; line-height: normal; color: var(--barbi-pink); ">{addn[$lang]}</h1>    
 </div>
 <div dir="rtl" class='textinput'>
   <input  bind:value={name_value} type='text' class='input' required>
-  <label for="name" class='label'>שם הערך</label>
+  <label for="name" class='label'>{valn[$lang]}</label>
   <span class='line'></span>
 </div>
-{#if shgi == true}<small class="text-red-600">השם כבר קיים</small>{/if}
+{#if shgi == true}<small class="text-red-600">{errmsg[$lang]}</small>{/if}
 <br />
 <div dir="rtl" class='textinput'>
   <input type='text' class='input' bind:value={desV} required>
-  <label for="descrip" class='label'>תיאור קצר</label>
+  <label for="descrip" class='label'>{des[$lang]}</label>
   <span class='line'></span>
 </div>
 
 <button style="margin-top: 20px; " on:click={addNewVall} 
-title="הוספה"
+title={btnTitles[$lang]}
 class=" hover:bg-barbi text-gold hover:text-mturk font-bold py-1 px-1 rounded-full"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
   <path fill="currentColor" d="M14.3 21.7C13.6 21.9 12.8 22 12 22C6.5 22 2 17.5 2 12S6.5 2 12 2C13.3 2 14.6 2.3 15.8 2.7L14.2 4.3C13.5 4.1 12.8 4 12 4C7.6 4 4 7.6 4 12S7.6 20 12 20C12.4 20 12.9 20 13.3 19.9C13.5 20.6 13.9 21.2 14.3 21.7M7.9 10.1L6.5 11.5L11 16L21 6L19.6 4.6L11 13.2L7.9 10.1M18 14V17H15V19H18V22H20V19H23V17H20V14H18Z" />
 </svg>
