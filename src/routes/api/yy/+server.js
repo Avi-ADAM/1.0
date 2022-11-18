a5c6b3a510d52e4c4f45489a7fda518e0428c43c
Lbaruch <aviadam.segel@gmail.com>
2022-11-18 20:43:21 +0200

    60.3 with messageback

--- src/routes/api/yy/+server.js
+++ src/routes/api/yy/+server.js
@@ -2,7 +2,19 @@ export const POST = async ({ request }) => {
     console.log("tryng", request.body)
     const data = await request.json()
     console.log("gg", data)
+    const name =""
+    const det =""
+    const action = "הרשמתך לעדכונים  התקבלה בהצלחה!"
+    const botMessage = `${name} %0A 
+     ${action} %0A 
+     ${det}`;
+    // %0A is url encoded '\n' which is used for new line. 
+    const Token = import.meta.env.VITE_TELEGRAM_BOT_TOKEN;
+    const chatId = data.message.from.id
+    console.log(chatId)
 
+    const url = `https://api.telegram.org/bot${Token}/sendMessage?chat_id=${chatId}&text=${botMessage}`;
+    const res = await fetch(url);
   //  const name = data.name;
   //  const job = data.job;
   //  console.log("ryng", name, job)
