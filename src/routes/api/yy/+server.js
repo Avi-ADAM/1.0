ee5c91902cbf0de315bf74d817534b870c37f918
Lbaruch <aviadam.segel@gmail.com>
2022-11-19 15:58:22 +0200

    60.4 more telgram test

--- src/routes/api/yy/+server.js
+++ src/routes/api/yy/+server.js
@@ -2,12 +2,24 @@ export const POST = async ({ request }) => {
     console.log("tryng", request.body)
     const data = await request.json()
     console.log("gg", data)
-    const name =""
-    const det =""
-    const action = "הרשמתך לעדכונים  התקבלה בהצלחה!"
+    const messegeText = data.message.text
+    const isReg = messegeText.startsWith('/getnutification')
+    const isShow = messegeText.startsWith('/showaddress')
+    let name,det,action;
+    if (isReg == true){
+     name =""
+     det =""
+     action = "הרשמתך לעדכונים  התקבלה בהצלחה!"
+    } else if (isShow == true){
+        name = ""
+        det = ""
+        action = "www.1lev1.world"
+    }
     const botMessage = `${name} %0A 
      ${action} %0A 
      ${det}`;
+
+     //getNutifiction => check for id and email and username alse ask for email and if exsist send to login with explanation how to reg
     // %0A is url encoded '\n' which is used for new line. 
     const Token = import.meta.env.VITE_TELEGRAM_BOT_TOKEN;
     const chatId = data.message.from.id
