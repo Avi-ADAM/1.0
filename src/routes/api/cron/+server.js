33ae3d93d3483b265e13868f67f44a90f4598ecf
Lbaruch <aviadam.segel@gmail.com>
2023-04-25 12:17:24 +0300

    67.1 cron with 2s delay

--- src/routes/api/cron/+server.js
+++ src/routes/api/cron/+server.js
@@ -1,9 +1,12 @@
 let url = "https://beoni.onrender.com/api/cuntries/5"
 let hook = "https://api.render.com/deploy/srv-ch2i7jrh4hsum46ol8t0?key=63ezRdymLCg"
 let t = false
-
+async function awaitapi(){
+const res = await fetch(url);
+return res
+}
 export async function GET(req) {
-    const res = await fetch(url);
+    const res = await awaitapi();
     if (res.status >= 200 && res.status < 300) {
         t = false
         console.log("ok")
@@ -11,8 +14,18 @@ export async function GET(req) {
       } else{
         if (t == false){
             console.log("problema")
-            t = true
+            let sec = false
+            setTimeout(async function(){
+                  const res = await awaitapi();
+              if (res.status >= 200 && res.status < 300) {
+        t = false
+        sec = false
+              }else{
+                 t = true
             const resi = await fetch(hook);
+              }
+      },2000)
+           
             console.log(resi)
         }
       }
