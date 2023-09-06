81dfdbadf7f6c400d8a0afc857602edd57bacaca
Lbaruch <aviadam.segel@gmail.com>
2023-09-06 15:26:36 +0300

    75.7 skil from json

--- src/lib/components/prPr/mi.js
+++ src/lib/components/prPr/mi.js
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
-
+import skill from '$lib/data/skills.json'
+import tafkidim from '$lib/data/tafkidim.json'
+import workways from '$lib/data/workways.json'
 export const mi = writable([])
-export const skil = writable([])
-export const ww = writable([])
-export const role = writable([])
+export const skil = writable(skill)
+export const ww = writable(workways);
+export const role = writable(tafkidim);
---
+++ src/lib/func/findSkillId.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+    export function find_skill_id(skill_name_arr,skills2,valc) {
+    var arr = [];
+    for (let j = 0; j < skill_name_arr.length; j++) {
+        for (let i = 0; i < skills2.length; i++) {
+            if (skills2[i].attributes[valc] === skill_name_arr[j]) {
+                arr.push(skills2[i].id);
+            }
+        }
+    }
+    return arr;
+};
+</script>
