77c6c314e3338777d753b0be241b2894b51b5b76
Lbaruch <aviadam.segel@gmail.com>
2023-10-23 00:51:11 +0300

    80.8 error  fix

---
+++ src/lib/func/calcX.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    export function calcX(restime){
+        let x = 0
+         if (restime == "feh") {
+        x = 48 * 60 * 60 * 1000
+    } else if (restime == "sth") {
+        x = 72 * 60 * 60 * 1000 
+    } else if (restime == "nsh") {
+        x = 96 * 60 * 60 * 1000
+    } else if (restime == "sevend") {
+        x = 168 * 60 * 60 * 1000
+    }
+    return x
+    }
+</script>
