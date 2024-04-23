d36ea59f44702e050e223b675c0b7e46f76081ca
Lbaruch <aviadam.segel@gmail.com>
2024-04-23 21:24:43 +0300

    92.9 try telegram buttons

---
+++ src/routes/api/newTelegram/+server.js
@@ -0,0 +1,36 @@
+//create a server with telegraf to listen for new messages
+import { Telegraf } from 'telegraf';
+import { Markup } from 'telegraf';
+let appIds = []
+//token new
+  const Token =  import.meta.env.VITE_TELEGRAM_BOT_TOKEN_NEW
+  const app = new Telegraf(Token);
+  app.start((ctx) => {
+    //check if the chat_id is in our list
+    if(appIds.includes(ctx.chat.id)){
+      ctx.reply('Welcome to 1💗1',{
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+        reply_markup: Markup.inlineKeyboard([
+          Markup.button.url("<<to 1💗1>>", 'https://1lev1.vercel.app/lev')
+        ])})
+    }else{
+     ctx.reply('Welcome to 1💗1', {
+       parse_mode: 'HTML',
+       disable_web_page_preview: true,
+       reply_markup: Markup.inlineKeyboard([
+         Markup.button.url(
+           '<<to registration להרשמה>>',
+           'https://1lev1.vercel.app'
+         ),
+         Markup.button.url(
+           '<<login להתחברות >>',
+           'https://1lev1.vercel.app/login'
+         ),
+         Markup.button.url(
+           '<<register for nutification הרשמה לעדכונים>>',
+           'https://1lev1.vercel.app/me'
+         )
+       ])
+     });}});
+  app.help((ctx) => ctx.reply('Send me a sticker'));
