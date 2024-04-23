6e62f65d07da2644a5649f20d6c83dabf9b2c4e7
Lbaruch <aviadam.segel@gmail.com>
2024-04-23 23:33:21 +0300

    93.4 another fix

--- src/routes/api/newTelegram/+server.js
+++ src/routes/api/newTelegram/+server.js
@@ -1,6 +1,46 @@
+//create a server with telegraf to listen for new messages
+import { Telegraf } from 'telegraf';
+import { Markup } from 'telegraf';
 
-// routes/+server.ts
-import bot from './bot';
+let appIds = [];
+//token new
+const Token = import.meta.env.VITE_TELEGRAM_BOT_TOKEN_NEW;
+const bot = new Telegraf(Token);
+
+bot.start((ctx) => {
+  console.log(ctx.chat.id);
+  //check if the chat_id is in our list
+  if (appIds.includes(ctx.chat.id)) {
+    ctx.reply('Welcome to 1💗1', {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true,
+      reply_markup: Markup.inlineKeyboard([
+        Markup.button.url('<<to 1💗1>>', 'https://1lev1.vercel.app/lev')
+      ])
+    });
+  } else {
+    ctx.reply('Welcome to 1💗1', {
+      parse_mode: 'HTML',
+      disable_web_page_preview: true,
+      reply_markup: Markup.inlineKeyboard([
+        Markup.button.url(
+          '<<to registration להרשמה>>',
+          'https://1lev1.vercel.app'
+        ),
+        Markup.button.url(
+          '<<login להתחברות >>',
+          'https://1lev1.vercel.app/login'
+        ),
+        Markup.button.url(
+          '<<register for nutification הרשמה לעדכונים>>',
+          'https://1lev1.vercel.app/me'
+        )
+      ])
+    });
+  }
+});
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+
 
 export async function POST({ request }) {
   try {
