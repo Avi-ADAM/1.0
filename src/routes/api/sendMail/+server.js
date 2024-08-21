e1c164ce05bd47f85083f759103ed3b57b86093d
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2024-08-21 21:56:03 +0300

    105 more mail vercel test

--- src/routes/api/sendMail/+server.js
+++ src/routes/api/sendMail/+server.js
@@ -1,60 +1,5 @@
 import nodemailer from 'nodemailer';
 
-async function sendMail(
-    emailHtml,
-    email,
-    previewText,
-    emailText) {
-     
-    const transporter = nodemailer.createTransport({
-            host: 'smtp.zoho.com',
-            secure: true,
-            port: 465,
-            auth: {
-              user: "notifications@1lev1.com",	
-              pass: import.meta.env.VITE_ZOHO,
-            },
-    });
-    console.log(import.meta.env.VITE_ZOHO, 'import.meta.env.VITE_ZOHO')
-    await new Promise((resolve, reject) => {
-        transporter.verify(function (err, success) {
-            if (err) {
-                console.log(err)
-                reject(err)
-            }
-            else {
-                console.log(success)
-                resolve(success)
-            }
-        });
-       })
-
-    const options = {
-        from: "notifications@1lev1.com",
-        to: email,
-        subject: previewText,
-        html: emailHtml,
-        text: emailText
-    };
-    await new Promise((resolve, reject) => {
-        transporter.sendMail(options, (err, info) => {
-            if (err) {
-                console.log(err)
-                reject(err)
-            }
-            else {
-                console.log(info)
-                resolve(info)
-            }
-        });
-
-    })
-
-    return 'Email sent successfully';
-}
-
-
-
 export async function POST({ request }) {
     const data = await request.json();
     console.log("bolkmail 44", data); 
@@ -63,17 +8,52 @@ export async function POST({ request }) {
     const email = data.email
     const previewText = data.previewText
 
-    try {
-        const result = await sendMail(
-            emailHtml,
-            email,
-            previewText,
-            emailText
-        );
-    
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        secure: true,
+        port: 465,
+        auth: {
+          user: "notifications@1lev1.com",	
+          pass: import.meta.env.VITE_ZOHO,
+        },
+});
+console.log(import.meta.env.VITE_ZOHO, 'import.meta.env.VITE_ZOHO')
+await new Promise((resolve, reject) => {
+    transporter.verify(function (err, success) {
+        if (err) {
+            console.log(err)
+            reject(err)
+        }
+        else {
+            console.log(success)
+            resolve(success)
+        }
+    });
+   })
+
+const options = {
+    from: "notifications@1lev1.com",
+    to: email,
+    subject: previewText,
+    html: emailHtml,
+    text: emailText
+};
+await new Promise((resolve, reject) => {
+    transporter.sendMail(options, (err, info) => {
+        if (err) {
+            console.log(err)
+            reject(err)
+        }
+        else {
+            console.log(info)
+            resolve(info)
+        }
+    });
+
+})
         return new Response(result);
-    } catch (error) {
+   /* } catch (error) {
         console.error('Error sending mail:', error);
         return new Response('Failed to send mail', { status: 500 });
-    }
+    }*/
 }
