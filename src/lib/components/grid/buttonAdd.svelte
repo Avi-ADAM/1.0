8ba6f0b5fa3414354afa9b5938b7e9d4c6d11619
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2024-10-30 05:15:16 +0200

    109.9 buttonAdd

---
+++ src/lib/components/grid/buttonAdd.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Button from "$lib/celim/ui/button.svelte";
+  import {lang} from '$lib/stores/lang'
+    export let text = {"he":"אני אבצע", "en":"assign to me"}
+    export let onClick = () => {}
+</script>
+<Button text={text[$lang]} on:click={onClick}/>
--- src/lib/components/grid/nameAndPname.svelte
+++ src/lib/components/grid/nameAndPname.svelte
@@ -1,13 +1,34 @@
 <script>
-    export let src = ''
-    export let pname = ''	
-    export let mname = ''
-</script>
-
-<div class="flex items-center gap-4">
-    <img class="w-10 h-10 rounded-full" {src} alt="project logo">
-    <div class="font-medium dark:text-gold">
-        <div>{pname}</div>
-        <div class="text-sm text-barbi">{mname}</div>
+    export let src = '';
+    export let pname = '';
+    export let mname = '';
+    export let type = '';
+    import Button from "$lib/celim/ui/button.svelte";
+import {lang} from '$lib/stores/lang'
+  export let text = {"he":"אני אבצע", "en":"assign to me"}
+  export let onClick = () => {}
+</script>{#if type !== "button"}
+<div class="flex items-center gap-2">
+    {#if src}
+        <img src={src} alt={pname} class="w-8 h-8 rounded-full" />
+    {/if}
+    <div class="flex flex-col text-start">
+        {#if pname}
+            <span>{pname}</span>
+        {/if}
+        {#if mname}
+            <span 
+                class="text-blue-500 cursor-pointer hover:underline" 
+                role="button"
+                tabindex=0
+                on:click={onClick}
+            
+            >
+                {mname}
+            </span>
+        {/if}
     </div>
 </div>
+{:else}
+<Button text={text} on:click={onClick}/>
+{/if}
