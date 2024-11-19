9b881e456c00270a6ccfbc7988208a18e9067489
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2024-11-19 23:26:09 +0200

    112.6 ceter for user

--- src/lib/components/footer/mobileFooter.svelte
+++ src/lib/components/footer/mobileFooter.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { lang } from '$lib/stores/lang.js';
   import { isChatOpen } from '$lib/stores/pendMisMes.js';
   import { navigating } from '$app/stores';
     import { page } from "$app/stores"
@@ -21,21 +22,22 @@
         console.log($navigating?.to)
 
     }
+    const brainLeb= {"he":"מוח הרקמות","en":"brain of organiczations"}
 </script>
 
 
 <div class="fixed z-50 w-full h-12 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-0 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-    <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+    <div class="grid h-full max-w-lg grid-cols-5 mx-auto" >
         <button on:click={()=>{
             activeRoute = "moach"
             goto("/moach")
         }} type="button" class="{$page.url.pathname == "/moach" ? "border-b-2 border-gold" : ""} inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
             <MoachIcon active={$navigating?.to.url.pathname == "/moach"}/>
-            <span class="sr-only">Home</span>
+            <span class="sr-only">{brainLeb[$lang]}</span>
         </button>
         <div id="tooltip-home" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-            Home
-            <div class="tooltip-arrow" data-popper-arrow></div>
+            {brainLeb[$lang]}
+            <div class="tooltip-arrow" ></div>
         </div>
         <button on:click={() => addi('chat')} type="button" 
             class="{$isChatOpen ? "border-b-2 border-gold" :""} inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
@@ -73,9 +75,10 @@
             activeRoute = "me"
             goto("/me")
         }} type="button" class="{$page.url.pathname == "/me" ? "border-b-2 border-gold" : ""} inline-flex flex-col items-center justify-center text-barbi px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-            <svg class="w-5 h-5 mb-1 text-barbi  group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-            </svg>
+              <img
+              class=""
+              src='https://res.cloudinary.com/love1/image/upload/v1641481504/newC_qq5z3l.svg'
+              alt="link to Profile">
             <span class="sr-only">Profile</span>
         </button>
         <div id="tooltip-profile" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
