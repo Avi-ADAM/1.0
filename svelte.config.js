926af1a343f334358d522787ec24b1fbc5d4180c
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2025-04-20 17:17:19 +0300

    116.7 timegrama ask add fetch adapter added node and static

--- svelte.config.js
+++ svelte.config.js
@@ -1,48 +1,59 @@
 import vercel from '@sveltejs/adapter-vercel';
-//import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+import staticAdapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
-///** @type {import('@sveltejs/kit').Config} */
-//let config;
-//if (import.meta.env.VITE_ENVI == "versel") {
- const config = {
+/** @type {import('@sveltejs/kit').Config} */
+let config;
+
+if (process.env.ADAPTER === 'vercel') {
+  config = {
     kit: {
       adapter: vercel({
         runtime: 'nodejs18.x'
-      }
-      )
+      })
     },
-   vite: {
-     optimizeDeps: {
-       include: ['just-throttle', 'dayjs']
-     }
-   },
+    vite: {
+      optimizeDeps: {
+        include: ['just-throttle', 'dayjs']
+      }
+    },
     preprocess: [preprocess({
       "postcss": true
     })]
   };
-//} else {
-//  config = {
-//    kit: {
-//      adapter: adapter({
-//        // default options are shown. On some platforms
-//        // these options are set automatically — see below
-//        pages: 'build',
-//        assets: 'build',
-//        fallback: null,
-//        precompress: false
-//      }),
-//      preprocess: [preprocess({
-//        "postcss": true
-//      })],
-//      prerender: {
-//        // This can be false if you're using a fallback (i.e. SPA mode)
-//        default: true
-//      }
-//    }
-//  };
-//}
+} else if (process.env.ADAPTER === 'static') {
+  config = {
+    kit: {
+      adapter: staticAdapter({
+        pages: 'build',
+        assets: 'build',
+        fallback: null,
+        precompress: false
+      }),
+      preprocess: [preprocess({
+        "postcss": true
+      })],
+      prerender: {
+        default: true
+      }
+    }
+  };
+} else {
+  config = {
+    kit: {
+      adapter: adapter({
+        out: 'build'
+      }),
+      preprocess: [preprocess({
+        "postcss": true
+      })]
+    }
+  };
+}
+
 export default config;
+
 // Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
