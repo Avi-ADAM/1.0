c1e43654dde383db554911386f5582c45055cea8
baruch babayof <58833138+Avi-ADAM@users.noreply.github.com>
2025-04-23 02:50:19 +0300

    119.8 pingrama

--- src/routes/api/pingrama/+server.js
+++ src/routes/api/pingrama/+server.js
@@ -1,14 +1,10 @@
 export async function GET({url, fetch}){
     try{
         let mode = url.searchParams.get('mode');
-        if (mode === null) {
-            console.error('No mode parameter found');
-            return new Response('No mode parameter found', { status: 400 });
-        }
         let add = mode === 'wake' ? '' : 'api/timegrama';
-        await fetch(import.meta.env.VITE_REND + add, {
+        fetch(import.meta.env.VITE_REND + add, {
             method: 'GET',
-    } );
+        }).catch(e => console.log(e));
     }catch(e){
         console.log(e)
     }
