f21ea10760644c77485cc5ee1971ec4919e37f5a
Lbaruch <aviadam.segel@gmail.com>
2025-08-27 20:40:54 +0300

    125.7 stat on false

--- src/lib/components/lev/cards/isScrolable.svelte.js
+++ src/lib/components/lev/cards/isScrolable.svelte.js
@@ -1,8 +1,8 @@
-export let isScrolable = $state({ value: true });
+export let isScrolable = $state({ value: false });
 import { isMobileOrTablet } from "$lib/utilities/device";
 export function toggleScrollable() {
     if (isMobileOrTablet()) {
       isScrolable.value = !isScrolable.value;
       console.log('isScrolable', isScrolable.value);
     }
-}
+}    
